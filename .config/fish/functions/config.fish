2512a698922614603ab52937f060d340e0f38719
Patrick Muldoon <doon@labratsoftware.com>
2024-12-02 19:54:17 -0800

    show status by default

--- .config/fish/functions/config.fish
+++ .config/fish/functions/config.fish
@@ -1,3 +1,7 @@
 function config --wraps='/usr/bin/git --git-dir=$HOME/.cfg/ --work-tree=$HOME' --wraps=git
-    /usr/bin/git --git-dir=$HOME/.cfg/ --work-tree=$HOME $argv
+    if count $argv >/dev/null
+        /usr/bin/git --git-dir=$HOME/.cfg/ --work-tree=$HOME $argv
+    else
+        /usr/bin/git --git-dir=$HOME/.cfg/ --work-tree=$HOME status
+    end
 end
