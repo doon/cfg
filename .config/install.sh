dd8b9d72feef3258159126d80d70b1c9733d0a77
Patrick Muldoon <doon@labratsoftware.com>
2025-05-09 20:23:50 -0700

    updated install.sh

--- .config/install.sh
+++ .config/install.sh
@@ -6,6 +6,12 @@ brew install fish-shell/fish-beta-4/fish
 echo /opt/homebrew/bin/fish | sudo tee -a /etc/shells
 chsh -s /opt/homebrew/bin/fish
 
+# install rust
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | s
+
+# add rust-analyzer
+rustup component add rust-analyzer
+
 #get stuff for nvim, and use nightly
 brew install wget
 brew install fd
@@ -15,5 +21,11 @@ brew install ripgrep
 brew install --cask font-fira-code
 brew install fzf
 brew install bat
+brew install python@3.13
+brew install pyright
+brew install tree-sitter
 brew tap benjiwolff/neovim-nightly
 brew install neovim-nightly
+
+# rebuild bat caches (colorscheme)
+bat cache --build
